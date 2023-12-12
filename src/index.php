68b7b8e58002a6a5b3a3a90a3601539578232d32
Manish Choudhary <choudharymanish@protonmail.com>
2023-12-12 18:31:37 +0530

    Fixed some UI issues

--- src/index.php
+++ src/index.php
@@ -1,83 +1,3 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__. "/../");
-$dotenv->load();
-
-$dotenv->required('APP_NAME')->notEmpty();
-$dotenv->required('AMPLITUDE_API_KEY')->notEmpty();
-
-function printForm($id) {
-  ?>
-  <form id="id_<?php echo $id; ?>" method="get" hx-get="/result.php" hx-trigger="submit" hx-target="#result_<?php echo $id; ?>">
-                <div class="flex flex-row xl:gap-9 gap-2">
-                  <div class="sm:col-span-2 sm:col-start-1">
-                    <div class="mt-2">
-                      <input
-                        type="number" 
-                        placeholder="Client ID"
-                        name="cid"
-                        id="cid"
-                        autocomplete="cid"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
-                         ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-                         focus:ring-2 focus:ring-inset focus:ring-indigo-600 
-                         sm:text-sm sm:leading-6 px-2">
-                    </div>
-                  </div>
-
-                  <div class="sm:col-span-2">
-                    <div class="mt-2">
-                      <input 
-                        type="number" 
-                        placeholder="Property ID" 
-                        name="property_id" 
-                        id="property_id" 
-                        autocomplete="property_id" 
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm 
-                        ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-                        focus:ring-2 focus:ring-inset focus:ring-indigo-600 
-                        sm:text-sm sm:leading-6 px-2">
-                    </div>
-                  </div>
-
-                  <div class="sm:col-span-2">
-                    <div class="mt-2">
-                      <input
-                        type="text" 
-                        name="device"
-                        id="device" 
-                        placeholder="Device"
-                        autocomplete="device"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm
-                        ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-                        focus:ring-2 focus:ring-inset focus:ring-indigo-600 
-                        sm:text-sm sm:leading-6 px-2">
-                    </div>
-                  </div>
-                  <div class="sm:col-span-2">
-                    <div class="mt-2">
-                      <input type="submit" name="check" id="check" autocomplete="check" class="block w-full rounded-md border-0 py-1.5 px-4 bg-blue-500 cursor-pointer text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    </div>
-                  </div> 
-                  <div class="sm:col-span-2">
-                    <div class="mt-2">
-                      <input 
-                      type="reset" 
-                      value="Clear"
-                      hx-get="/clear.php" hx-trigger="click" hx-target="#result_<?php echo $id; ?>"
-                      name="code" id="code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 px-4 bg-gray-700 cursor-pointer text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    </div>
-                  </div> 
-                  <div class="sm:col-span-2">
-                    <div id="result_<?php echo $id; ?>" class="mt-2">
-                        
-                    </div>
-                  </div> 
-                </div>
-                </form>
-  <?php
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -92,28 +12,22 @@ function printForm($id) {
         <div class=" flex flex-row mx-auto container text-center">
             <a href="/" class="text-white p-4 text-2xl text-bold">Feature Flag Demo</a>
         </div>
-   
     </nav>
-    <main class="mx-auto container">
+    <main class="mx-auto lg:container">
         <div class="flex flex-row justify-center">
-            <div class="flex flex-col w-full mx-auto">
-                <div class="border mt-4 py-4 rounded-xl w-1/3 mx-auto ">
+            <div class="flex flex-col w-full mx-auto max-lg:m-8">
+                <div class="border mt-4 py-4 rounded-xl xl:w-1/3 w-2/3 mx-auto ">
                     <h1 class="text-4xl font-bold text-center mb-4">Check Feature Flags</h1>
                 </div>
-                <div class="mx-auto m-10 border p-8 rounded shadow">
-                <?php
-                
-                printForm(1);
-                printForm(2);
-                printForm(3);
-                printForm(4);
-                printForm(5);
-                ?>
-                </div>
-                
-                
+                <div class="mx-auto m-10 border p-4 rounded shadow divide-y">
+                  <div hx-get="/row.php" hx-trigger="load" hx-target="this" class="py-4">Loading</div>
+                  <div hx-get="/row.php" hx-trigger="load" hx-target="this" class="py-4">Loading</div>
+                  <div hx-get="/row.php" hx-trigger="load" hx-target="this" class="py-4">Loading</div>
+                  <div hx-get="/row.php" hx-trigger="load" hx-target="this" class="py-4">Loading</div>
+                  <div hx-get="/row.php" hx-trigger="load" hx-target="this" class="py-4">Loading</div>
+                 
+                </div>   
             </div>
     </main>
-
 </body>
 </html>
--- src/result.php
+++ src/result.php
@@ -1,3 +1,33 @@
 <?php
-    print_r($_GET)
+require_once __DIR__ . '/bootstrap.php';
+
+function areYouReady($cid, $property_id, $device) {
+    // (1) Initialize the experiment client
+    $experiment = new \AmplitudeExperiment\Experiment();
+    $client = $experiment->initializeRemote($_ENV['AMPLITUDE_API_KEY']);
+
+    // (2) Fetch variants for a user
+    $user = \AmplitudeExperiment\User::builder()
+        ->userProperties(['cid' => $cid, 'property_id' => $property_id, 'device' => $device]) 
+        ->build();
+    $variants = $client->fetch($user)->wait();
+
+    // (3) Access a flag's variant
+    $variant = $variants['awesome-feature'] ?? null;
+    if ($variant && $variant->value == 'on') {
+       return true;
+    }
+    return false;
+}
+
+$cid = $_GET['cid'] ?? null;
+$property_id = $_GET['property_id'] ?? null;
+$device = $_GET['device'] ?? 'Desktop';
+
+$ready = areYouReady($cid, $property_id, $device);
+if ($ready) {
+    echo "🥳 Access Granted ✅";
+} else {
+    echo "😞 Access Denied 🚫";
+}
 ?>
